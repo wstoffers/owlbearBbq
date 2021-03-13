150daf870ccfef9d325fb3c7d1d03d4b84599c00
Will S <73315256+wstoffers@users.noreply.github.com>
2021-03-12 20:16:49 -0700

    Added kilo integration test

--- factor0/src/cloudStorage/test_getBucketSize.py
+++ factor0/src/cloudStorage/test_getBucketSize.py
@@ -57,16 +57,30 @@ def test_formString_iec(rawDataLakeBucket):
 def test_withUnitsFormStringIntegration_mega(rawDataLakeBucket):
     sizes = [29881909, 0]
     array = rawDataLakeBucket._withUnits(sizes)
-    #np.testing.assert_almost_equal(array,
-    #                               np.array([['29.88 MB', '28.50 MiB'],
-    #                                         ['0 B','0 B']]))
-    #^^^only use this with arrays of floats/doesn't work with strings
     assert array[0][0] == '29.88 MB'
     assert array[0][1] == '28.50 MiB'
     assert array[1][0] == '0 B'
     assert array[1][1] == '0 B'
 
 def test_withUnitsFormStringIntegration_kilo(rawDataLakeBucket):
+    sizes = [81909, 0]
+    array = rawDataLakeBucket._withUnits(sizes)
+    #np.testing.assert_almost_equal(array,
+    #                               np.array([['29.88 MB', '28.50 MiB'],
+    #                                         ['0 B','0 B']]))
+    #^^^only use this with arrays of floats/doesn't work with strings
+    assert array[0][0] == '81.91 KB'
+    assert array[0][1] == '79.99 KiB'
+    assert array[1][0] == '0 B'
+    assert array[1][1] == '0 B'
+
+def test_withUnitsFormStringIntegration_byte(rawDataLakeBucket):
+    pass
+
+def test_withUnitsFormStringIntegration_giga(rawDataLakeBucket):
+    pass
+
+def test_withUnitsFormStringIntegration_tera(rawDataLakeBucket):
     pass
 
 #run:
