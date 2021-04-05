899f3ad3d0460e9a40e9aa3df0aebdaaf3083ef8
Will S <73315256+wstoffers@users.noreply.github.com>
2021-04-05 13:17:59 -0600

    Refactoring bucket tests into small and medium

--- factor0/tests/googSmall/test_getBucketSize.py
+++ factor0/tests/googSmall/test_getBucketSize.py
@@ -54,35 +54,6 @@ def test_formString_iec(rawDataLakeBucket):
     strings = rawDataLakeBucket._formString(filtered,sizes,keys,ending)
     assert strings == ['25.58 MiB', '0 B']
 
-def test_withUnitsFormStringIntegration_mega(rawDataLakeBucket):
-    sizes = [29881909, 0]
-    array = rawDataLakeBucket._withUnits(sizes)
-    assert array[0][0] == '29.88 MB'
-    assert array[0][1] == '28.50 MiB'
-    assert array[1][0] == '0 B'
-    assert array[1][1] == '0 B'
-
-def test_withUnitsFormStringIntegration_kilo(rawDataLakeBucket):
-    sizes = [81909, 0]
-    array = rawDataLakeBucket._withUnits(sizes)
-    #np.testing.assert_almost_equal(array,
-    #                               np.array([['29.88 MB', '28.50 MiB'],
-    #                                         ['0 B','0 B']]))
-    #^^^only use this with arrays of floats/doesn't work with strings
-    assert array[0][0] == '81.91 KB'
-    assert array[0][1] == '79.99 KiB'
-    assert array[1][0] == '0 B'
-    assert array[1][1] == '0 B'
-
-def test_withUnitsFormStringIntegration_byte(rawDataLakeBucket):
-    pass
-
-def test_withUnitsFormStringIntegration_giga(rawDataLakeBucket):
-    pass
-
-def test_withUnitsFormStringIntegration_tera(rawDataLakeBucket):
-    pass
-
 #run:
 if __name__ == '__main__':
     pass
