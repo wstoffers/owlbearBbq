d58c160fd62023dca18c11f76b94f3de53d4a5f0
Will S <73315256+wstoffers@users.noreply.github.com>
2021-04-24 02:02:07 -0600

    Updating Cloud Function path

--- factor0/src/cloudScheduler/schedule.sh
+++ factor0/src/cloudScheduler/schedule.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 gcloud app create --region=us-central
-gcloud scheduler jobs create http acquireDataJob --schedule "* * * * *" --uri "https://us-central1-owlbear-bbq.cloudfunctions.net/acquireData"
+gcloud scheduler jobs create http acquireDataJob --schedule "* * * * *" --uri "https://us-central1-owlbear-barbecue.cloudfunctions.net/acquireData"
